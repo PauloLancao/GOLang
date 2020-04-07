7df8ea05881deda82010d72ce3aa011ffde0479d
Paulo Lancao <Paulo.Lancao@bjss.com>
2020-04-07 16:22:14 +0100

    first go commit

---
+++ src/strings/strings.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// RemoveIndexAlt version 1
+func RemoveIndexAlt(s []int, index int) []int {
+	ret := make([]int, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
+// RemoveIndex version 0
+func RemoveIndex(s []int, index int) []int {
+	return append(s[:index], s[index+1:]...)
+}
+
+func main() {
+
+	// A `const` statement can appear anywhere a `var`
+	// statement can.
+	const n = 500000000
+
+	// A number can be given a type by using it in a
+	// context that requires one, such as a variable
+	// assignment or function call. For example, here
+	// `math.Sin` expects a `float64`.
+	fmt.Println(math.Sin(n))
+
+	arr := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+	fmt.Println("Arr full ", arr)
+
+	arr = RemoveIndex(arr, 3)
+
+	fmt.Println("Arr remove index 3 ", arr)
+
+	var arr1 = RemoveIndexAlt(arr, 1)
+
+	fmt.Println("Arr1 remove index 1 ", arr1)
+	fmt.Println("Arr full ", arr)
+
+	sum := 0
+	for _, num := range arr {
+		sum += num
+	}
+
+	fmt.Println("sum:", sum)
+}
