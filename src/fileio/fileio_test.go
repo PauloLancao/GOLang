435e711d34e32e78dcc8ed94468a9902a4bbaad9
Paulo Lancao <Paulo.Lancao@bjss.com>
2020-04-08 17:26:43 +0100

    code complete exercise 8

--- src/fileio/fileio_test.go
+++ src/fileio/fileio_test.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStringArrToByteArrAndBack(t *testing.T) {
+	content, err := stringArrToByteArr()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, content)
+
+	contentStr := byteArrToStringArr(content)
+
+	assert.NotNil(t, contentStr)
+
+	assert.Equal(t, cities, contentStr)
+}
