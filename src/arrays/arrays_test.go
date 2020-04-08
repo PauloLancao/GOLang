504cf5245d6f7b9f8c3d181c07da5d25e6397999
Paulo Lancao <Paulo.Lancao@bjss.com>
2020-04-08 13:31:49 +0100

    Move tests to another package

---
+++ src/arrays/arrays_test.go
@@ -0,0 +1,21 @@
+package arrays
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var arr = []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+func TestRemoveIndexAlt(t *testing.T) {
+	var retArr = RemoveIndexAlt(arr, 4)
+
+	assert.Equal(t, []int{0, 1, 2, 3, 5, 6, 7, 8, 9}, retArr)
+}
+
+func TestRemoveIndex(t *testing.T) {
+	var retArr = RemoveIndex(arr, 7)
+
+	assert.Equal(t, []int{0, 1, 2, 3, 4, 5, 6, 8, 9}, retArr)
+}
