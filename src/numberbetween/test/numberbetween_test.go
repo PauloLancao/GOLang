1e122d8e5911ccdccd2311395ac22783a33e8ab8
Paulo Lancao <Paulo.Lancao@bjss.com>
2020-04-08 10:08:05 +0100

    Exercise 3 complete

---
+++ src/numberbetween/test/numberbetween_test.go
@@ -0,0 +1,23 @@
+package numberbetween_test
+
+import (
+	"bytes"
+	"strconv"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStdIoInput(t *testing.T) {
+	var stdin bytes.Buffer
+
+	stdin.Write([]byte("222\n"))
+
+	result, err := stdin.ReadString('\n')
+	assertResult, err1 := strconv.Atoi(strings.Replace(result, "\n", "", -1))
+
+	assert.NoError(t, err)
+	assert.NoError(t, err1)
+	assert.Equal(t, 222, assertResult)
+}
