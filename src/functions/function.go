5b1c82662d5aabe2b4c0274ca5f2eb4fcd37587f
Paulo Lancao <Paulo.Lancao@bjss.com>
2020-04-14 16:56:49 +0100

    Rename

--- src/functions/function.go
+++ src/functions/function.go
@@ -1,5 +1,80 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func hypot(x, y float64) float64 {
+	return math.Sqrt(x*x + y*y)
+}
+
+// squares returns a function that returns
+// the next square number each time it is called.
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
+}
+
+func main() {
+	fmt.Println("Hypot: ", hypot(3, 4)) // "5"
+
+	f := squares()
+	fmt.Println("Square func: ", f()) // "1"
+	fmt.Println("Square func: ", f()) // "4"
+	fmt.Println("Square func: ", f()) // "9"
+	fmt.Println("Square func: ", f()) // "16"
+
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
 }
