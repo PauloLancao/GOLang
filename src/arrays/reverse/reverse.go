275ee2c432144d75738ba1b31801b144eb87e271
Paulo Lancao <Paulo.Lancao@bjss.com>
2020-04-14 14:37:57 +0100

    reverse code

--- src/arrays/reverse/reverse.go
+++ src/arrays/reverse/reverse.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func reverse(s []int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
@@ -12,6 +15,24 @@ func reversePtr(s *[]int) {
 	for i, j := 0, len(*s)-1; i < j; i, j = i+1, j-1 {
 		(*s)[i], (*s)[j] = (*s)[j], (*s)[i]
 	}
+}
+
+func rotate(s []int, idxSlice int, left bool) []int {
+	var size = len(s)
+	var res = make([]int, size)
+
+	if idxSlice > size {
+		log.Fatal("IdxSlice out of bounds")
+	} else {
+		if left {
+			// rotate left
+			res = append(s[idxSlice:], s[:idxSlice]...)
+		} else {
+			// rotate right
+			res = append(s[size-idxSlice:], s[:size-idxSlice]...)
+		}
+	}
+	return res
 }
 
 func main() {
@@ -38,4 +59,11 @@ func main() {
 	reverse(s[2:])
 
 	fmt.Println(s) // "[2 3 4 5 0 1]"
+
+	r := []int{0, 1, 2, 3, 4, 5, 8, 9}
+	left := false
+	idx := 2
+	fmt.Printf("Rotate array original \nLEFT -> %t: \nIdx -> %d \n%v \nresult: \n%v",
+		left, idx, r, rotate(r, idx, left))
+
 }
